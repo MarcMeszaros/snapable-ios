9d162d035b038c071946f17798d3744c181ed9f7
Marc Meszaros <marc@snapable.com>
2012-07-30 13:03:20 -0400

    Initial empty iOS project

---
+++ Snapable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Snapable
+//
+//  Created by Marc Meszaros on 12-07-30.
+//  Copyright (c) 2012 Snapable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Snapable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Snapable
+//
+//  Created by Marc Meszaros on 12-07-30.
+//  Copyright (c) 2012 Snapable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
