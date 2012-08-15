e8e143f6582d8ab4fdff6fc34f24a50387ae4003
Marc Meszaros <marc@snapable.com>
2012-08-15 12:27:56 -0400

    Fix the date format for events in the event list

--- Snapable/Classes/Controllers/SnapEventListViewController.m
+++ Snapable/Classes/Controllers/SnapEventListViewController.m
@@ -87,49 +87,14 @@ static NSString *cellIdentifier = @"eventListCell";
     
     // Configure the cell...
     cell.uiEventTitle.text = event.title;
-    cell.uiEventDate.text = [startDate descriptionWithLocale:[NSLocale currentLocale]];
+    NSDateFormatter *eventDateFormat = [[NSDateFormatter alloc] init];
+    [eventDateFormat setDateFormat:@"EEE MMMM d, h:mm a"];
+    
+    cell.uiEventDate.text = [eventDateFormat stringFromDate:startDate];
+    //cell.uiEventDate.text = [startDate descriptionWithLocale:[NSLocale currentLocale]];
 
     return cell;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
