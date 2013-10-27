525fd69127ae4429834112e98ed94cec5a12b36f
Marc Meszaros <marc@snapable.com>
2013-10-27 18:44:51 -0400

    Fix the loading screen assets

--- Snapable/Classes/Controllers/SnapLoadingViewController.m
+++ Snapable/Classes/Controllers/SnapLoadingViewController.m
@@ -21,21 +21,6 @@
 @synthesize locationController;
 @synthesize results;
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
-
 - (void)viewDidUnload
 {
     [super viewDidUnload];
